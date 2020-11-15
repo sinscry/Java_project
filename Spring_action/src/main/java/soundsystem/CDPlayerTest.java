fd304bf944fb0ee63cfd1653c31263b3adf25dd5
sinscry <sinscryw@gmail.com>
2020-11-15 23:24:39 +0800

    spring实战第四版

---
+++ Spring_action/src/main/java/soundsystem/CDPlayerTest.java
@@ -0,0 +1,21 @@
+package soundsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = CDPlayerConfig.class)
+public class CDPlayerTest {
+    @Autowired
+    private CompactDisc cd;
+    @Test
+    public void cdShouldNotBeBull(){
+        assertNotNull(cd);
+    }
+}
